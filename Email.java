7ce9248ac744fba1c3aaeeddf45fd618fe326f87
Mayolov <danyvalencia25@gmail.com>
2022-07-14 17:10:58 -0700

    version 1 of EmailApp Future changes will see: - A better terminal UI - Allowing there to not be duplicate emails - Double password authentication for resetting password - Database that saves new users emails and passwords - Etc

---
+++ Email.java
@@ -0,0 +1,95 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Email {
+    private String firstName;
+    private String lastName;
+    private String password;
+    private int defaultPasswordLength = 10;
+    private String department;
+    private String email;
+    private int mailboxCapacity = 500;
+    private String alternateEmail;
+    private String companysuffix = "gmail.com";
+
+    // constructor
+    public Email(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+
+        // Call a method asking for the department and returns the department
+        this.department = setDepartment();
+
+        // Call a method that returns a random password
+        this.password = randomPassword(defaultPasswordLength);
+        System.out.println("Your password is: " + this.password);
+
+        // Combine elements to generate Email
+        email = firstName.toLowerCase() + "." + lastName.toLowerCase() + "@" + department + "." + companysuffix;
+    }
+
+    // generate password
+    private String randomPassword(int length) {
+        String passwordSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%";
+
+        char[] password = new char[length];
+        for (int i = 0; i < length; i++) {
+            int rand = (int) (Math.random() * passwordSet.length());
+            password[i] = passwordSet.charAt(rand);
+        }
+
+        return new String(password);
+    }
+
+    // Ask department
+    private String setDepartment() {
+        System.out.println("New employee " + firstName
+                + "Enter department Code: \n1 for Sales \n2 for Development \n3 for accounting\n0 for none");
+        Scanner input = new Scanner(System.in);
+        int depChoice = input.nextInt();
+
+        if (depChoice == 1) {
+            return "Sales";
+        } else if (depChoice == 2) {
+            return "Dev";
+        } else if (depChoice == 3) {
+            return "Accounting";
+        } else {
+            return "";
+        }
+    }
+
+    // set mailbox cap
+    public void setMailBoxCapacity(int capacity) {
+        this.mailboxCapacity = capacity;
+    }
+
+    public int getMailBoxCapacity() {
+        return mailboxCapacity;
+    }
+
+    // set alt email
+    public void setAltEmail(String altEmail) {
+        this.alternateEmail = altEmail;
+    }
+
+    public String getAltEmail() {
+        return alternateEmail;
+    }
+
+    // change password
+    public void changePassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String showInfo() {
+        return "Display NAME: " + firstName + " " + lastName +
+                "\nCOMPANY EMAIL: " + email +
+                "]nMAILBOX CAPACITY: " + mailboxCapacity + "mb";
+
+    }
+}
---
+++ EmailApp.java
@@ -0,0 +1,11 @@
+/**
+ * EmailApp
+ */
+public class EmailApp {
+    public static void main(String[] args) {
+
+        Email em1 = new Email("Jon", "Smith");
+        System.out.println(em1.showInfo());
+    }
+
+}
